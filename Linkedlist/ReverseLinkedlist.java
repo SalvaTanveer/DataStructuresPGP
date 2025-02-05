29a0a1c8138280ea91a6bbdc0b0d361d518e34d0
Salva Tanveer <salvatanveer701@gmail.com>
2025-02-05 16:13:16 +0530

    Reverse a linkedlist

--- Linkedlist/ReverseLinkedlist.java
+++ Linkedlist/ReverseLinkedlist.java
@@ -1,34 +1,21 @@
 package LinkedList;
 
-/*  Definition for singly-linked list.
-class ListNode {
-    int val;
-    ListNode next;
-
-    ListNode() {
-    }
-
-    ListNode(int val) {
-        this.val = val;
-    }
-
-    ListNode(int val, ListNode next) {
-        this.val = val;
-        this.next = next;
-    }
-}*/
-
 public class ReverseLinkedlist {
     public ListNode reverseList(ListNode head) {
+        // if node is null of has only one node
         if (head == null || head.next == null) {
             return head;
         }
         ListNode temp = head;
         ListNode prev = null;
         while (temp != null) {
+            // storing the next node
             ListNode nextNode = temp.next;
+            // pointing the current node to previous node
             temp.next = prev;
+            // making the current node as previous
             prev = temp;
+            // making the next node as current node
             temp = nextNode;
         }
         head = prev;
