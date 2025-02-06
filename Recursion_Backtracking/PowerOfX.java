fc3339b54dadab56ef4367405a4e34ae8a044fe2
Salva Tanveer <salvatanveer701@gmail.com>
2025-02-06 21:25:36 +0530

    Power(x,n)

---
+++ Recursion_Backtracking/PowerOfX.java
@@ -0,0 +1,26 @@
+
+package Recursion_Backtracking;
+
+public class PowerOfX {
+    public double myPow(double x, int n) {
+        long N = n;
+        if (N < 0) {
+            x = 1 / x;
+            N = -n;
+        }
+        return pow(x, N);
+    }
+
+    public double pow(double x, long n) {
+        if (n == 0)
+            return 1;
+        if (n == 1)
+            return x;
+        double half = pow(x, n / 2);
+        if (n % 2 == 0) {
+            return half * half;
+        } else {
+            return half * half * x;
+        }
+    }
+}
