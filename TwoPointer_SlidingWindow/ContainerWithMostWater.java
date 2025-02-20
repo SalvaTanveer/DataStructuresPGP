3daa4631a150b5211c93940dcb68841fb0580546
Salva Tanveer <salvatanveer701@gmail.com>
2025-02-20 19:40:06 +0530

    ContainerWithMostWater

---
+++ TwoPointer_SlidingWindow/ContainerWithMostWater.java
@@ -0,0 +1,18 @@
+package TwoPointer_SlidingWindow;
+
+public class ContainerWithMostWater {
+    public int maxArea(int[] height) {
+        int i = 0, j = height.length - 1;
+        int ans = Integer.MIN_VALUE;
+        while (i < j) {
+            int water = (j - i) * Math.min(height[i], height[j]);
+            ans = Math.max(ans, water);
+            if (height[i] <= height[j]) {
+                i++;
+            } else {
+                j--;
+            }
+        }
+        return ans;
+    }
+}
